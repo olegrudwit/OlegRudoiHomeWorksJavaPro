9605adda5de7b33da5b327c818ac4b17c5a5ae5e
olegrudwit <olegrudwit@gmail.com>
2022-10-30 21:18:28 +0200

    hw 10 init commit

---
+++ src/apps/MultithreadingCollection/Main.java
@@ -0,0 +1,11 @@
+package apps.MultithreadingCollection;
+
+/**
+ *  @author Oleg Rudoi
+ *  @version 1.0 29 Oct 2022
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
